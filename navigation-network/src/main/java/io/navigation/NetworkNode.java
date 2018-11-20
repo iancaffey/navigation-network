8a352c5952232218ae71fdfe1f6d8c187e09e0b5
Ian Caffey <iancaffey@ymail.com>
2018-11-19 22:41:34 -0600

    Added gson support for NetworkNode.

--- navigation-network/src/main/java/io/navigation/NetworkNode.java
+++ navigation-network/src/main/java/io/navigation/NetworkNode.java
@@ -1,9 +1,12 @@
 package io.navigation;
 
+import org.immutables.gson.Gson;
+
 /**
  * @author Ian Caffey
  * @since 1.0
  */
+@Gson.ExpectedSubtypes({Station.class, Stop.class})
 public interface NetworkNode {
     String getId();
 }
